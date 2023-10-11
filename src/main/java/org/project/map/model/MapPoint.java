cad762ed21874e6c1b0d9727ed693aa16ca99043
Jakub Sepiolo <5sepij35@solent.ac.uk>
2023-10-11 22:11:44 +0100

    Base project files

---
+++ src/main/java/org/project/map/model/MapPoint.java
@@ -0,0 +1,110 @@
+/*
+Copyright [yyyy] [name of copyright owner]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.project.map.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class MapPoint {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+    private double lat;
+    private double lng;
+
+
+    @SuppressWarnings("unused")
+    public MapPoint()
+    {
+    }
+
+    public MapPoint(String name, String description, String category, double lat, double lng){
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Map point: {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", lat=" + lat +
+                ", lng=" + lng +
+                '}';
+    }
+}
