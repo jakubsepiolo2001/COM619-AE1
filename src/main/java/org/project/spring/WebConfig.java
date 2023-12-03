a2a893c4f8657a2cffbacdf76edecf38abe936d2
Jakub Sepiolo <5sepij35@solent.ac.uk>
2023-12-03 12:51:41 +0000

    Basic navbar language

---
+++ src/main/java/org/project/spring/WebConfig.java
@@ -0,0 +1,26 @@
+package org.project.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+import java.util.TimeZone;
+@Configuration
+public class MyBeansConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        localeResolver.setDefaultTimeZone(TimeZone.getTimeZone("UTC"));
+        return localeResolver;
+    }
+
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("localeData");
+        return localeChangeInterceptor;
+    }
+}
