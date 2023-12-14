f030cf723ebe3d17c4e282fbeb39fc612dd653d5
Jakub Sepiolo <5sepij35@solent.ac.uk>
2023-12-14 12:04:09 +0000

    Licensing to all java code

--- src/main/java/org/project/user/controller/MVCController.java
+++ src/main/java/org/project/user/controller/MVCController.java
@@ -1,3 +1,20 @@
+/*
+Copyright 2023 Git Guardians
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 package org.project.user.controller;
 
 import java.io.PrintWriter;
@@ -45,7 +62,7 @@ public class MVCController {
         return sessionUser;
     }
 
-    // this redirects calls to the root of our application to index.html
+    // this redirects calls to the root of our application to
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
     public String index(Model model) {
         return "home";
